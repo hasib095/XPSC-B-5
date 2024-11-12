3444be8de2e835394918da25264d3a001c565bf2
hasib095 <cse1095.hasib@gmail.com>
2024-11-12 23:44:40 +0600

    Day3

---
+++ Week 3/Day 3/Chess_Olympiad.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dl;
+#define endl "\n"
+#define vl vector<long long>
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define arr(a,n)  for(ll i =0; i < n; i++) cin >> a[i];
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define srt(v)  sort(v.begin(), v.end());
+#define rsrt(v)  sort(v.rbegin(), v.rend());
+const int mod = 1e9+7;
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    int s = a + b + c;
+    dl cnt1 = 0, cnt2 = 0;
+    dl ad = (4 - s) ;
+    cnt1 = abs((a  + b * 0.5)+ad ); 
+    cnt2 = abs((b * 0.5 + c ) );
+    if (cnt1 > cnt2)
+        yes;
+    else
+        no;
+    // cout<<cnt1<<" "<<cnt2<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    solve();
+    // int t; cin >> t; while(t--){solve();}
+    return 0;
+}
