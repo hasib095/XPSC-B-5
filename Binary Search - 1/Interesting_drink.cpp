ed07337b594f9325ec7a7252942fc4789ef3e930
hasib095 <cse1095.hasib@gmail.com>
2025-01-27 23:46:45 +0600

    Binarysearch

---
+++ Binary Search - 1/Interesting_drink.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define all(x) begin(x), end(x)
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> v(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    sort(all(v));
+    ll q;
+    cin >> q;
+    for (ll i = 0; i < q; i++)
+    {
+        ll val;
+        cin >> val;
+        auto up = upper_bound(all(v), val);
+        ll ans = up - v.begin();
+        cout << ans << endl;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    solve();
+    // int t; cin >> t; while(t--){solve();}
+    return 0;
+}
