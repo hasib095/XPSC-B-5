a4cab7339afb0278cc3cb986403688b9a95ecfa4
hasib095 <cse1095.hasib@gmail.com>
2025-02-17 00:14:09 +0600

    week13

---
+++ Week 13/Are You a Robot.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+#define ll long long
+#define endl "\n"
+#define all(x) begin(x), end(x)
+
+void solve()
+{
+ cout<<"safety"<<endl;
+}
+int main()
+{   
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE 
+    freopen("in.txt" , "r", stdin);
+    freopen("out.txt" , "w", stdout);
+    #endif
+    solve();
+    //int t; cin >> t; while(t--){solve();}
+    return 0;
+}
---
+++ Week 13/Is it stated.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define all(x) begin(x), end(x)
+
+void solve()
+{
+    string s;
+    cin >> s;
+    auto it = s.find("it");
+    if (it != string::npos)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    // solve();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
