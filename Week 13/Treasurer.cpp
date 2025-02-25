6bc75e70ac3070cd28558adfc1a15d21572b9cc5
hasib095 <cse1095.hasib@gmail.com>
2025-02-25 23:41:25 +0600

    week13

---
+++ Week 13/Treasurer.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define all(x) begin(x), end(x)
+
+void solve()
+{
+    int n;
+    string s;
+    cin >> n >> s;
+    ll a = count(all(s), '-');
+    ll b = n - a;
+    cout << (a / 2) * b * ((a + 1) / 2) << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    // solve();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
