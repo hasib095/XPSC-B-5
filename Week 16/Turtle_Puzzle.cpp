b158b3e9a3f736b03a05de2843b2c47d493657bb
hasib095 <cse1095.hasib@gmail.com>
2025-03-03 02:38:00 +0600

    Week16

---
+++ Week 16/Turtle_Puzzle.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+#define all(x) begin(x), end(x)
+
+void solve()
+{
+    int n, sum = 0;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        sum += abs(a[i]);
+    }
+    cout << sum << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    // solve();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
